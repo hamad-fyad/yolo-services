877dcfdfea6a80697bb4ee14c9dfedca684f562c
hamad-fyad <hamad-fyad@users.noreply.github.com>
2025-08-20 08:12:26 +0000

    deploy: 95c99c8cbdbf32ca9c08b49c137fbff3f88bd82e

--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-@app.on_event("startup")
-# def on_startup():
-#     init_db() # pragma: no cover
--- tests/helper_function.py
+++
@@ -1,7 +0,0 @@
-import base64 
-
-def get_auth_headers(username, password):
-        # Encode username:password in base64 for Basic Auth header
-        credentials = f"{username}:{password}"
-        encoded_credentials = base64.b64encode(credentials.encode()).decode()
-        return {"Authorization": f"Basic {encoded_credentials}"}
